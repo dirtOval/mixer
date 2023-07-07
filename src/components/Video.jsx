da41b6ee2a8c45aafc64a248f1a1de0d39b950a1
deirdre <deirdre.loyall@gmail.com>
2023-07-06 20:31:28 -0400

    added react-youtube, got YouTube component working in Video component

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import YouTube from 'react-youtube';
 
 const Video = (props) => {
+
+  const opts = {
+    height: '390',
+    width: '640',
+  };
+
   return (
     <div className="video">
-      video :P
+      <YouTube videoId="H96RoJKqbN4" opts={opts} />
     </div>
   );
 }
