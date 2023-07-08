dbe96e13168b740118e45e5a7218969b5bbae954
deirdre <deirdre.loyall@gmail.com>
2023-07-08 16:17:02 -0400

    test Modal now opens on search form submit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Modal from 'react-modal';
 
-const SearchBar = (props) => {
+const SearchBar = ({setVideoId}) => {
+
+  const modalStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+  
+  const [search, setSearch] = useState('');
+  const [modalOpen, setModalOpen] = useState(false);
+  const [searchResults, setSearchResults] = useState({});
+
+  const onChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const openModal = (e) => {
+    e.preventDefault();
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   return (
     <>
-      <input class="search-bar" />
+      <form onSubmit={openModal}>
+        <input className="search-bar" onChange={onChange} />
+      </form>
+      <Modal 
+       isOpen={modalOpen}
+       onRequestClose={closeModal}
+       style={modalStyles}
+       contentLabel={'Search'}>
+        <button onClick={closeModal}>close</button>
+        <h1>OH YEAH ITS SEARCHING TIME</h1>
+      </Modal>
     </>
   )
 }
