642da18c902156397563cd8ea784e83dc37b1dda
deirdre <deirdre.loyall@gmail.com>
2023-07-23 22:16:47 -0400

    now doesn't download yt-dlp.exe unless it doesn't exist

--- server/index.js
+++ server/index.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const YTDlpWrap = require('yt-dlp-wrap').default;
+const fs = require('fs');
 const app = express();
 const port = 6969;
 // const {ytDlp} = require('./ytDlp.js');
 
 
 app.get('/audio', async (req, res) => {
+  console.log('lets fucking rock');
   res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
   // let data = {
   //   data: ''
   // }
   let githubReleasesData = await YTDlpWrap.getGithubReleases(1, 5);
-  await YTDlpWrap.downloadFromGithub();
+  if (!fs.existsSync('./yt-dlp.exe')) {
+    await YTDlpWrap.downloadFromGithub();
+  }
   console.log('got YTDlp!');
   const ytDlp = await new YTDlpWrap('./yt-dlp.exe');
   setTimeout(() => {
+    console.log('downloading');
     let stream = ytDlp.execStream([
       'https://www.youtube.com/watch?v=' + 'H96RoJKqbN4',
       '--extract-audio'
@@ -27,7 +32,7 @@ app.get('/audio', async (req, res) => {
       // "'%(id)s.%(ext)s'"
     ])
     stream.pipe(res);
-  }, 5000)
+  }, 3000)
   // console.log(JSON.stringify(ytDlp));
 })
 
